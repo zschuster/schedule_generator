995a4d0e015ffca82674827ee23b5fbe0cf8d2be
Zach Schuster <zts@Zachs-MacBook-Pro.local>
2019-10-24 21:53:23 -0400

    read in dynamo objects

--- lambda/update_practice_drill_dynamo.py
+++ lambda/update_practice_drill_dynamo.py
@@ -3,8 +3,14 @@ import urllib.parse
 import boto3
 import pandas as pd
 
-print('Loading function')
-s3 = boto3.client('s3')
+
+def dynamo_scan_to_df(dict_list):
+    base_dict = {k: [v] for k, v in dict_list[0].items()}
+    keys = base_dict.keys()
+    for i in range(1, len(dict_list)):
+        for key in keys:
+            base_dict[key].append(dict_list[i][key])
+    return base_dict
 
 
 def scan_dynamo_table(table_name='practice_drill'):
@@ -12,24 +18,36 @@ def scan_dynamo_table(table_name='practice_drill'):
     drill_table = dynamo_resource.Table(table_name)
 
     response = drill_table.scan()
-    return response
+    return dynamo_scan_to_df(response['Items'])
 
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
 
+    s3 = boto3.client('s3')
     # Get the object from the event and show its content type
-    # bucket = event['Records'][0]['s3']['bucket']['name']
-    # key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    # try:
-    #     response = s3.get_object(Bucket=bucket, Key=key)
-    #     print("CONTENT TYPE: " + response['ContentType'])
-    #     return response['ContentType']
-    # except Exception as e:
-    #     print(e)
-    #     print('Error getting object {} from bucket {}. Make sure they\
-    #      exist and your bucket is in the same region as this function.'.format(key, bucket))
-    #     raise e
-    #
-    # practice_drill_data = scan_dynamo_table()
-    # return practice_drill_data
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they\
+         exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
+
+    input_data = pd.read_csv(response['Body'])
+    print(input_data.info())
+    practice_drill_data = scan_dynamo_table()
+    practice_drill_data = pd.DataFrame(practice_drill_data)
+    print(practice_drill_data.info())
+
+    return
+
+
+if __name__ == '__main__':
+    with open('update_test_event.json', 'r') as f:
+        test_event = json.load(f)
+
+    print(lambda_handler(test_event, None))
+
